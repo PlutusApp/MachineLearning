db6648c27727f0777af0021dda6781a267a3f622
Aung Myat <amyat@purdue.edu>
2017-03-26 11:34:25 -0400

    Fix sugg

--- suggestion.py
+++ suggestion.py
@@ -15,57 +15,57 @@ def main():
             if (price <= 0.1*MI):
                 if (rating <= 1):
                     sentence = "This is a product of fair price and good quality which is suitable for your saving habit."
-                    result = [sentence , "good-buy"]
+                    result = [sentence , "good-buy" , "done"]
                     print(result);
                     return;
                 else:
                     sentence = "This is a product of fair price but the quality cannot be guaranteed."
-                    result = [sentence , "good-buy"]
+                    result = [sentence , "good-buy" , "done"]
                     print(result);
                     return;
             elif price > 0.1*MI and price < 0.4*MI:
                 if rating >= 2:
                     sentence = "The item is not fully encouraged to buy. The suggestion is to look for other products or save for later and better use."
-                    result = [sentence , "average-buy"]
+                    result = [sentence , "average-buy" , "warning"]
                     print(result);
                     return;
                 else:
                     sentence = "This is a good product but you are encouraged to consider if you really need it."
-                    result = [sentence , "average-buy"]
+                    result = [sentence , "average-buy" , "warning"]
                     print(result);
                     return;
             else:
                 sentence = "You should reconsider about your saving habit. If the item is not essential, there might be a better way you can use the money."
-                result = [sentence , "bad-buy"]
+                result = [sentence , "bad-buy" , "not_interested"]
                 print(result);
                 return;
         elif usertype == 1:
             if (price < 0.3*MI):
                 if (rating <= 2):
                     sentence = "This is a product of fair price and good quality which is suitable for your saving habit."
-                    result = [sentence , "good-buy"]
+                    result = [sentence , "good-buy" , "done"]
                     print(result);
                     return;
                 else:
                     sentence = "This is a product of fair price but the quality cannot be guaranteed."
-                    result = [sentence , "good-buy"]
+                    result = [sentence , "good-buy" , "done"]
                     print(result);
                     return;
             elif price >= 0.3*MI and price <= 0.6*MI:
                 if rating >= 2:
                     sentence = "The item is not fully encouraged to buy. The suggestion is to look for other products or save for later and better use."
-                    result = [sentence , "average-buy"]
+                    result = [sentence , "average-buy" , "warning"]
                     print(result);
                     return;
                 else:
                     sentence = "This is a good product but you are encouraged to consider if you really need it."
-                    result = [sentence , "average-buy"]
+                    result = [sentence , "average-buy" , "warning"]
                     print(result);
                     return;
             else:
                 if rating == 0:
                     sentence = "If this is a good investment for you, think deeply about it."
-                    result = [sentence , "bad-buy"]
+                    result = [sentence , "bad-buy" , "not_interested"]
                     print(result);
                     return;
                 else:
@@ -77,28 +77,28 @@ def main():
             if price <= 0.2*MI:
                 if rating <=2:
                     sentence = "This is a product of fair price and good quality which is suitable for your saving habit."
-                    result = [sentence , "good-buy"]
+                    result = [sentence , "good-buy" , "done"]
                     print(result);
                     return;
                 else:
                     sentence = "This is a product of fair price but the quality cannot be guaranteed."
-                    result = [sentence , "good-buy"]
+                    result = [sentence , "good-buy" , "done"]
                     print(result);
                     return;
             elif price > 0.2*MI and price <= 0.6*MI:
                 if rating >= 2:
                     sentence = "The item is not fully encouraged to buy. The suggestion is to look for other products or save for later and better use."
-                    result = [sentence , "average-buy"]
+                    result = [sentence , "average-buy" , "warning"]
                     print(result);
                     return;
                 else:
                     sentence = "This is a good product but you are encouraged to consider if you really need it."
-                    result = [sentence , "average-buy"]
+                    result = [sentence , "average-buy" , "warning"]
                     print(result);
                     return;
             else:
                 sentence = "You should reconsider about your saving habit. If the item is not essential, there might be a better way you can use the money."
-                result = [sentence , "bad-buy"]
+                result = [sentence , "bad-buy" , "not_interested"]
                 print(result);
                 return;
 
